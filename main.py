2b17223ff2d4a350c48c517518695af3ed1dc02c
DickslonKo <91776851+DickslonKo@users.noreply.github.com>
2022-01-29 17:03:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ import redis
 app = Flask(__name__)
 @app.route("/Hello World/")
 def Hello_World():
-    return("Hello World")
+    #Ask Redis, does x exist?
+    #if not exist, return stored value
+    #if not exist, calculate
+    x = 0
+    for i in range (1000000):
+        result = x + 1
+    return result
 
